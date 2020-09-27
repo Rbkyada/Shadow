b16a78ee2295299fea9278446ca5025f0604de27
Kaal007 <kaalspat@gmail.com>
2020-09-27 19:03:28 +0530

    Updates

---
+++ precache-manifest.dd1458941288310cbdb1e03baa8563e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d670039e9b4478c7775c59032cc2263",
+    "url": "/Shadow/index.html"
+  },
+  {
+    "revision": "5b11c787929d527b85e9",
+    "url": "/Shadow/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "10b69aa07e043c0efdab",
+    "url": "/Shadow/static/css/main.d1bd2a39.chunk.css"
+  },
+  {
+    "revision": "5b11c787929d527b85e9",
+    "url": "/Shadow/static/js/2.6e319c59.chunk.js"
+  },
+  {
+    "revision": "c17f465735f6a300dcc13ebb10c34821",
+    "url": "/Shadow/static/js/2.6e319c59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10b69aa07e043c0efdab",
+    "url": "/Shadow/static/js/main.e4f1e3e4.chunk.js"
+  },
+  {
+    "revision": "16bc5e1009131d3c54b2",
+    "url": "/Shadow/static/js/runtime-main.50e2e067.js"
+  },
+  {
+    "revision": "5d1a7c95a045bae3fa006285e8203b66",
+    "url": "/Shadow/static/media/sound.5d1a7c95.mp3"
+  }
+]);
